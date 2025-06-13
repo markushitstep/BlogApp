a9f8a20449b511524be872e80bea1fdb1a62446a
Max <markushitstep@gmail.com>
2025-06-13 12:33:50 +0300

    add Tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from '../src/store';
+import './index.css';
 
 
 const root = ReactDOM.createRoot(
