0cccfff11758ad001a8fb9dd7ab2d410da33e0ae
Max <markushitstep@gmail.com>
2025-06-20 16:49:58 +0300

    install Toast

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { BlogsList } from './components/BlogsList';
 import Header from './components/Header';
 import { BlogForm } from './components/BlogForm';
 import { BlogDetails } from './components/BlogsDetails';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <ToastContainer position="bottom-right" />
       <Routes>
         <Route path="/" element={<BlogsList />} />
         <Route path="/blogs" element={<BlogsList />} />
